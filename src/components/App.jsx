b4b1ba17be9c92eeec8d228e993e56ecdda44da7
Phazerous <nphazerous@gmail.com>
2022-12-10 13:53:20 +0600

    Card | Mapping elements

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,15 +2,20 @@ import React from "react";
 import Card from "./Card";
 import contacts from "../contacts";
 
-const contact1 = contacts[0];
-const contact2 = contacts[1];
-const contact3 = contacts[2]; 
+function createCard(contact) {
+  return (
+    <Card
+      name={contact.name}
+      imgURL={contact.imgURL}
+      phone={contact.phone}
+      email={contact.email}
+    />
+  );
+}
 
 function App() {
   return <div>
-    <Card name={contact1.name} imgURL={contact1.imgURL} phone={contact1.phone} email={contact1.email}/>
-    <Card name={contact2.name} imgURL={contact2.imgURL} phone={contact2.phone} email={contact2.email}/>
-    <Card name={contact3.name} imgURL={contact3.imgURL} phone={contact3.phone} email={contact3.email}/>
+    {contacts.map(createCard)}
   </div>
 }
 
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,16 +1,19 @@
 import React from "react";
+import Avatar from "./Avatar";
+import Detail from "./Detail";
+
 
 function Card(props) {
   return (
     <div className='card'>
       <div className="top">
         <p className="name">{props.name}</p>
-        <img className='circle-img' src={props.imgURL} alt="avatar.png" />
+        <Avatar img={props.imgURL} />
       </div>
 
       <div className='bottom'>
-      <p className='info'>{props.phone}</p>
-      <p className='info'>{props.email}</p>
+        <Detail detailInfo={props.phone} />
+        <Detail detailInfo={props.email} />
       </div>
     </div>
   );
