2fa7d33206142d065acc65cea2217d30905fb300
Phazerous <nphazerous@gmail.com>
2022-12-10 17:05:45 +0600

    Time Clock

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,21 +1,16 @@
 import React, { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [time, setTime] = useState(new Date().toLocaleTimeString())
 
-  function increase() {
-    setCount(count + 1);
-  }
+  setInterval(() => setTime((new Date()).toLocaleTimeString()), 1000);
 
-  function decrease() {
-    setCount(count - 1);
-  }
-
-  return <div className="container">
-    <h1>{count}</h1>
-    <button onClick={decrease}>-</button>
-    <button onClick={increase}>+</button>
-  </div>
+  return (
+    <div className="container">
+      <h1>{time}</h1>
+      <button>Time</button>
+    </div>
+  );
 }
 
 export default App;
