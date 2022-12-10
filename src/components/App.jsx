0a0b47f9e38cb4126d54afa0377a165bd970eb8b
Phazerous <nphazerous@gmail.com>
2022-12-11 00:43:46 +0600

    Spread Operator Practice

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,35 +1,35 @@
 import React, { useState } from "react";
+import Heading from "./Heading";
 
 function App() {
-  const [contact, setContact] = useState({
-    fName: "",
-    lName: "",
-    email: ""
-  });
+  const [noteName, setNoteName] = useState("");
+  const [noteList, setNoteList] = useState([]);
+
+  function handleClick() {
+    setNoteList([...noteList, noteName]);
+    setNoteName('');
+  }
 
   function handleChange(e) {
-    const { value, name } = e.target;
-
-    setContact((prevValue) => {
-      return {
-        ...prevValue, // spread operator
-        [name]: value // we put the name of our input field in the square brackets, so the JS will interpretate input field with its name and not "name" property.
-      }
-    })
+    setNoteName(e.target.value);
   }
 
   return (
     <div className="container">
-      <h1>
-        Hello {contact.fName} {contact.lName}
-      </h1>
-      <p>{contact.email}</p>
-      <form>
-        <input onChange={handleChange} name="fName" placeholder="First Name" />
-        <input onChange={handleChange} name="lName" placeholder="Last Name" />
-        <input onChange={handleChange} name="email" placeholder="Email" />
-        <button>Submit</button>
-      </form>
+      <Heading />
+      <div className="form">
+        <input type="text" onChange={handleChange} value={noteName} />
+        <button onClick={handleClick}>
+          <span>Add</span>
+        </button>
+      </div>
+      <div>
+        <ul>
+          {noteList.map(note => {
+            return <li>{note}</li>;
+          })}
+        </ul>
+      </div>
     </div>
   );
 }
