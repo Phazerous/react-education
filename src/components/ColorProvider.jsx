0f81bbb248e6a0c3f2aa664cbf5aea1241f611bc
Phazerous <nphazerous@gmail.com>
2022-12-13 01:13:46 +0600

    ColorProvider P.1 | Created

---
+++ src/components/ColorProvider.jsx
@@ -0,0 +1,34 @@
+import React, { createContext, useState } from "react";
+import { nanoid } from "nanoid";
+import colorData from "../color-data.js";
+
+const ColorContext = createContext();
+
+export default function ColorProvider({ children }) {
+  const [colors, setColors] = useState(colorData);
+
+  const addColor = (title, color) => {
+    const updatedColor = [...colors, {
+      id: nanoid(),
+      title,
+      color,
+      rating: 0
+    }];
+    setColors(updatedColor);
+  }
+
+  const removeColor = (id) => {
+    const updatedColors = colors.filter(color => color.id !== id);
+    setColors(updatedColors);
+  }
+
+  const rateColor = (id, rating) => {
+    const updatedColors = colors.map(color => color.id === id ? { ...color, rating } : color);
+    setColors(updatedColors);
+  }
+
+  return <ColorContext.Provider value={{ colors, addColor, removeColor, rateColor }}>
+    {children}
+  </ColorContext.Provider>
+}
+
